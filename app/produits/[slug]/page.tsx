39d3d27597b4358852b5385abcce43ffad677310
Bamba24 <155769054+Bamba24@users.noreply.github.com>
2025-07-12 21:47:53 +0200

    correction du bug

--- app/produits/[slug]/page.tsx
+++ app/produits/[slug]/page.tsx
@@ -1,6 +1,7 @@
+
 import ProductPage from '../components/productPage';
 
-export default async function Page({ params }: { params: Promise<{ slug: string }> }) {
-  const slug = (await params).slug;
+export default  function Page({ params }: { params: { slug: string } }) {
+  const slug = params.slug;
   return <ProductPage params={{ slug }} />;
 }
